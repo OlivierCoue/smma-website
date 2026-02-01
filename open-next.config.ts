451a95e60ff7db2650f5e2797d92f8ad0eb5a2c1
Skel <leroy.etienne@gmail.com>
2026-02-02 00:09:18 +0100

    chore: remove R2 cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,4 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
+// minimal open-next config without R2 incremental cache
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
-});
+export default defineCloudflareConfig({});
